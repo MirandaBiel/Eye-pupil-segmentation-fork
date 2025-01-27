79528b1e152ea4de2291e195b782ebf497554a4e
MirandaBiel <gabrieldaconceicaomiranda@gmail.com>
2025-01-27 20:31:14 -0300

    Ajuste RAM

--- video_tracking.py
+++ video_tracking.py
@@ -22,12 +22,13 @@ else:
         try:
             # Executar o rastreamento no vídeo atual com rastreador especificado
             results = model.track(
-                source=video_path,   # Caminho para o vídeo
-                conf=0.5,            # Limite de confiança (ajuste conforme necessário)
-                iou=0.5,             # Limite de IOU (ajuste conforme necessário)
-                tracker="botsort.yaml",  # Use "bytetrack.yaml" ou "botsort.yaml" conforme necessário
-                show=False,          # Não mostrar o vídeo em tempo real
-                save=True            # Salvar o vídeo anotado no diretório padrão de saída
+                source=video_path,      # Caminho para o vídeo
+                conf=0.5,               # Limite de confiança (ajuste conforme necessário)
+                iou=0.5,                # Limite de IOU (ajuste conforme necessário)
+                tracker="botsort.yaml", # Use "bytetrack.yaml" ou "botsort.yaml" conforme necessário
+                show=False,             # Não mostrar o vídeo em tempo real
+                save=True,              # Salvar o vídeo anotado no diretório padrão de saída
+                stream=True             # Desativa o uso da RAM
             )
             print(f"Rastreamento concluído para: {video_path}")
         except Exception as e:
